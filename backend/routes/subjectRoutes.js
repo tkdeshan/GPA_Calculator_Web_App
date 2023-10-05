01062476072601b04d448f1c590b72df7bd5d391
tkdeshan <tkdeshan1103@gmail.com>
2023-10-05 20:17:54 +0530

    Complete API

--- backend/routes/subjectRoutes.js
+++ backend/routes/subjectRoutes.js
@@ -4,27 +4,50 @@ const Subjects = require('../modules/subjectModule');
 
 // Read all data
 router.get('/sub', (req, res) => {
-  res.send('get');
+  Subjects.find({})
+    .then((subjects) => res.json(subjects))
+    .catch((err) => res.json(err));
 });
 
 // Read data from id
 router.get('/sub/:id', (req, res) => {
-  res.send('get id');
+  const id = req.params.id;
+  Subjects.findById({ _id: id })
+    .then((subjects) => res.json(subjects))
+    .catch((err) => res.json(err));
 });
 
 // Insert data
 router.post('/sub', (req, res) => {
-  res.send('insert ');
+  Subjects.create(req.body)
+    .then((subjects) => res.json(subjects))
+    .catch((err) => res.json(err));
 });
 
 // Update data
 router.put('/sub/:id', (req, res) => {
-  res.send('update');
+  const id = req.params.id;
+  Subjects.findByIdAndUpdate(
+    { _id: id },
+    {
+      subCode: req.body.subCode,
+      subName: req.body.subName,
+      credit: req.body.credit,
+      year: req.body.year,
+      semester: req.body.semester,
+      grade: req.body.grade,
+    }
+  )
+    .then((subjects) => res.json(subjects))
+    .catch((err) => res.json(err));
 });
 
 // Delete data
 router.delete('/sub/:id', (req, res) => {
-  res.send('delete');
+  const id = req.params.id;
+  Subjects.findByIdAndDelete({ _id: id })
+    .then((res) => res.json(res))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
