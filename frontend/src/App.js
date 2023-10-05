d6a9079f21ded21896cf418eabeeb7776a71a1a2
tkdeshan <tkdeshan1103@gmail.com>
2023-10-05 21:47:51 +0530

    Modify home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import Home from './components/Home';
 import AddSubject from './components/AddSubject';
 import UpdateSubject from './components/UpdateSubject';
 
-
 function App() {
   return (
-    <div>
+    <div className='bg-primary'>
+      <div className="container col-md-6 bg-white rounded p-3">
+        <nav class="navbar rounded">
+          <div class="container-fluid">
+            <h1 class="navbar-brand">GPA Calculator</h1>
+            <button class="btn btn-success" type="submit">
+              Add +
+            </button>
+          </div>
+        </nav>
+      </div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/create' element={<AddSubject />}></Route>
-          <Route path='/update/:id' element={<UpdateSubject />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create" element={<AddSubject />}></Route>
+          <Route path="/update/:id" element={<UpdateSubject />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,164 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Home() {
+  const BaseURL = 'http://localhost:5000';
+  const [subjects, setSujects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BaseURL}/api/sub/`)
+      .then((result) => setSujects(result.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className="d-flex bg-primary justify-content-center align-items-center">
+      <div className="col-md-6 bg-white rounded p-3">
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Subject Code</th>
+                <th>Subject Name</th>
+                <th>Grade</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <p class="fw-semibold">1 year | 1 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 1 && subject.semester === 1)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">1 year | 2 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 1 && subject.semester === 2)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">2 year | 1 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 2 && subject.semester === 1)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">2 year | 2 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 2 && subject.semester === 2)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">3 year | 1 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 3 && subject.semester === 1)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">3 year | 2 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 3 && subject.semester === 2)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">4 year | 1 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 4 && subject.semester === 1)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+            <p class="fw-semibold">4 year | 2 semester</p>
+            <tbody>
+              {subjects
+                .filter((subject) => subject.year === 4 && subject.semester === 2)
+                .map((subject) => (
+                  <tr key={subject._id}>
+                    <td>{subject.subCode}</td>
+                    <td>{subject.subName}</td>
+                    <td>{subject.grade}</td>
+                    <td>
+                      <button>Update</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
