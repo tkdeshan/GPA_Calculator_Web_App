07e7abf353c75b25ef3bab83c69534d4c19e9bb9
tkdeshan <tkdeshan1103@gmail.com>
2023-10-05 20:06:36 +0530

    Create mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const subjectRoutes = require('./routes/subjectRoutes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 const PORT = 5000;
 
+app.use(cors());
+app.use(express.json());
 app.use('/api', subjectRoutes);
+
+const dbURI = process.env.MONGODB_URI;
+
+async function connectToMongoDB() {
+    try {
+      // Attempt to connect to MongoDB using Mongoose
+      await mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('MongoDB connection error:', error);
+      // Exit the application if the connection fails
+      process.exit(1);
+    }
+  }
+  
+  // Call the async function to connect to MongoDB
+  connectToMongoDB();
 
 app.listen(PORT, () => {
   console.log(`The server is running http://localhost:${PORT}`);
